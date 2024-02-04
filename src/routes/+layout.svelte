d4159a62cbc91c4dc81e8c7316a7049ab404aaa5
DoChau <dntamchau@gmail.com>
2024-02-04 18:45:33 +0700

    run no style '

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Header from '$lib/Header/index.svelte';
+  import '../app.css';
+</script>
+
+<Header />
+
+<main class="page text-center my-0 mx-auto p-4">
+  <slot />
+</main>
+
+<style global lang="scss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
